cf31816062c652d305682cd6e00d8ce8a1efb99b
raj720s <raj720s@raj.com>
2022-02-02 10:30:03 +0530

    added all

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.use("/api/sort", sortApiRoutes);
 // });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/appy/build/index.html")));
+  app.use(express.static(path.join(__dirname, "/appy/build/")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "appy", "build", "index.html"));
   });
